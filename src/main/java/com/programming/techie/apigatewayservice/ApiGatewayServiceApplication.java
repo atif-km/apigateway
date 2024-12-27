8127a2bff41c7cd05f906d67c3f6e8f1b6c1c6ee
atif-kma <kloudatif@gmail.com>
2024-12-27 16:08:39 +0530

    Initial commit

---
+++ src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package com.programming.techie.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
+}
+
